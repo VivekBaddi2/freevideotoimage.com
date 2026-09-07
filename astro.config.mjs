6b14bea9d9e0a656f67c61e5b42558a4d647d3f3
Vivek <vivekbaddi2@gmail.com>
2026-09-07 15:58:42 +0530

    added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://freevideotoimage.com',
-  integrations: [preact()],
+  integrations: [
+    preact(),
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
